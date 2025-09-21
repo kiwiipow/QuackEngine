ed37403f8a43149468f33c3f17f9c4bc18e94383
Wakiren <javipedre4@gmail.com>
2025-09-21 14:09:06 +0200

    Empty CMake Project

---
+++ QuackEngine/QuackEngine.cpp
@@ -0,0 +1,12 @@
+﻿// QuackEngine.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "QuackEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
