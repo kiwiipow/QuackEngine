6a143a7914e24ff932a42363bd09f0994e315321
Wakiren <javipedre4@gmail.com>
2025-09-26 10:33:53 +0200

    Minor Change

--- QuackEngine.cpp
+++ QuackEngine.cpp
@@ -24,9 +24,9 @@ int main()
         return -1;
     }
 
-    // Setup Min/Major version for using OpenGL 4
+    // Setup Min/Major version for using OpenGL 4.6
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
 
     // Set Core Profile Mode
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
